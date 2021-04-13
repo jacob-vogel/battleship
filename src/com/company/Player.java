cbd1457944ab13f2cee63d5b710721d25a735ad5
Jacob Vogel <jakevogel33@gmail.com>
2021-04-13 15:55:23 -0600

    Player Class Updated: Still incomplete, doesn't know how to interact with server or what messages are being recieved. startBattleship: Calls the player() constructor, should be good to go.

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -6,46 +6,94 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Player {
     private Socket comm;
     private boolean connected;
-    private boolean whichPlayer;
+    //private boolean whichPlayer;// true -> player1 false -> player2
     private PrintWriter writer;
     private BufferedReader socketReader;
     private InputStreamReader stream;
 
-    public Player(boolean playerNum){
+    public Player(){
         connected = false;
-        whichPlayer = playerNum;
-        this.attemptToConnect();
-        if(connected){
-            try {
-                String playerMsg = socketReader.readLine();
+        boolean stay = true;
+        Scanner reader = new Scanner(System.in);
+        String response;
+        while(stay) {
+            this.attemptToConnect();
+            if (connected) {
+                boolean endGame = false;
+                while(!endGame) {
+                    try {
+                        System.out.println("Connection Successful!");
+                        String playerMsg = socketReader.readLine();
+                        System.out.println(playerMsg);
+                        if (playerMsg.equals("endGame")) {
+                            endGame = true;
+                        } else {
+                            response = reader.nextLine();
+                            writer.print(response);
+                            writer.flush();
+                        }
+                        //maybe check to see if a response is warranted don't want to always respond
+                /*
+                STEPS FOR CLIENT SERVER INTERACTION (sketch)
+                1. user starts game via startBattleship class
+                    1a. startBattleship creates a new Player() class with constructor
+                2. Player() constructor
+                    2a. player attempts to connect to server
+                3. Successful connection
+                    3a. player is assigned number (player 1 or 2) by server
+                    3b. player sets its whichPlayer value
+                    3c. server sends "guess?" / "endGame" / "otherPlayer"
+                        3ci. if guess then get guess from console
+                        3cii. if endGame then exit program
+                        3ciii. if otherPlayer notify user that it is otherPlayer's turn
+                4. Unsuccessful connection
+                    4a. display unsuccessful connection to user
+                    4b. ask if they would like to connect again or exit
+                    4c. if exit then exit program
+                    4d. if not exit
+                 */
 
-            } catch (IOException e) {
-                e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Thanks for playing Battleship!");
+            } else {
+                System.out.println("ERROR: Unable to connect to server");
+                System.out.println("Would you like to try again?");
+                response = reader.nextLine();
+                response = response.toLowerCase();
+                //System.out.println(response);
+                if(response.equals("yes")){
+                    stay = true;
+                }else {
+                    System.out.println("Thanks for Playing!");
+                    stay = false;
+                }
             }
-        }
-        else{
-
         }
     }
 
     public void attemptToConnect(){
         System.out.println("Attempting to connect to host with port 5000");
         try{
-            comm = new Socket(127.0.0.1, 5000);
+            comm = new Socket("127.0.0.1", 5000);
             writer = new PrintWriter(comm.getOutputStream());
             stream = new InputStreamReader(comm.getInputStream());
             socketReader = new BufferedReader(stream);
             writer.flush();
             connected = true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            connected = false;
         } catch (UnknownHostException e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            connected = false;
+        } catch (IOException e) {
+            //e.printStackTrace();
             connected = false;
         }
     }
---
+++ src/com/company/startBattleship.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class startBattleship {
+    public static class Main {
+        public static void main(String[] args){
+            startBattleship starter = new startBattleship();
+            starter.go();
+        }
+    }
+    public void go(){
+        Player player = new Player();
+    }
+}
